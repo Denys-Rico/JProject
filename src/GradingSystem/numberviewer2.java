4d5a94f17520b168680f48fd00f959bdacf550e1
Denys-Rico <576dnys.ken@Gmail.com>
2025-07-29 21:07:42 +0800

    Updating Java..

--- src/GradingSystem/numberviewer2.java
+++ src/GradingSystem/numberviewer2.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class numberviewer2 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int number;
-
-        while (true) {
-            System.out.print("Enter a number (type -1 to exit): ");
-            number = input.nextInt();
-
-            if (number == -999) {
-                System.out.println("Exiting program. Goodbye!");
-                break;
+        try (Scanner input = new Scanner(System.in)) {
+            int number;
+            
+            while (true) {
+                System.out.print("Enter a number (type -1 to exit): ");
+                number = input.nextInt();
+                
+                if (number == -999) {
+                    System.out.println("Exiting program. Goodbye!");
+                    break;
+                }
+                
+                if (number > 0) {
+                    System.out.println("\n" + number + " is a positive number");
+                } else if (number < 0) {
+                    System.out.println("\n" + number + " is a negative number");
+                } else {
+                    System.out.println("\nInputted number Zero");
+                }
+                
+                System.out.println();
             }
-
-            if (number > 0) {
-                System.out.println("\n" + number + " is a positive number");
-            } else if (number < 0) {
-                System.out.println("\n" + number + " is a negative number");
-            } else {
-                System.out.println("\nInputted number Zero");
-            }
-
-            System.out.println(); 
-        }
 
         input.close();
+        }
     }
 }
